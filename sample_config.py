49a96088f74bdbba1c61ef650673846194ca1176
mohanrc1515 <98520369+mohanrc1515@users.noreply.github.com>
2025-04-29 13:58:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,36 +4,26 @@ import os
 
 class Config(object):    
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7696984863:AAEiUA76NTYiQ2dYlCzxEAaymT_FMnnkKpM")
 
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "12655645"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "05c4cafe00b81ed83207bb4365e0053b")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1025922801").split())
     
     # Banned Users...
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    #BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
     # Channels to forward the formatted video (Optional, Prefix: "-100", Bot should be an admin of the channels)
-    CHANNEL1_ID = os.environ.get("CHANNEL1_ID")
+    CHANNEL1_ID = os.environ.get("CHANNEL1_ID", "1714238387")
 
-    CHANNEL1_NAME = os.environ.get("CHANNEL1_NAME")
+    CHANNEL1_NAME = os.environ.get("CHANNEL1_NAME", "Mohanrajanrc")
 
     CHANNEL2_ID = os.environ.get("CHANNEL2_ID")
 
-    CHANNEL2_NAME = os.environ.get("CHANNEL2_NAME")
+    #CHANNEL2_NAME = os.environ.get("CHANNEL2_NAME")
 
-    CHANNEL3_ID = os.environ.get("CHANNEL3_ID")
-
-    CHANNEL3_NAME = os.environ.get("CHANNEL3_NAME")
-
-    CHANNEL4_ID = os.environ.get("CHANNEL4_ID")
-
-    CHANNEL4_NAME = os.environ.get("CHANNEL4_NAME")
-
-    CHANNEL5_ID = os.environ.get("CHANNEL5_ID")
-
-    CHANNEL5_NAME = os.environ.get("CHANNEL5_NAME")
+    
