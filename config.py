927411aba0bee2c0ebcc321536a4b711d07d399f
mohanrc1515 <98520369+mohanrc1515@users.noreply.github.com>
2025-04-29 13:54:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Development(Config):
   APP_ID = "12655645"
-  API_HASH =05c4cafe00b81ed83207bb4365e0053b "eb064548abfb49dc3eeb1aeb98ae0f581e"
+  API_HASH = "05c4cafe00b81ed83207bb4365e0053b"
   TG_BOT_TOKEN = "7696984863:AAEiUA76NTYiQ2dYlCzxEAaymT_FMnnkKpM"
   AUTH_USERS = [1025922801]
   CHANNEL1_ID = "-1001714238387"                            
