0ed01a110544d885f83bb436aa69c6899f1c674d
mohanrc1515 <98520369+mohanrc1515@users.noreply.github.com>
2025-04-29 14:11:01 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-class Development(Config):
-  APP_ID = "12655645"
-  API_HASH = "05c4cafe00b81ed83207bb4365e0053b"
-  TG_BOT_TOKEN = "7696984863:AAEiUA76NTYiQ2dYlCzxEAaymT_FMnnkKpM"
-  AUTH_USERS = [1025922801]
-  CHANNEL1_ID = "-1001714238387"                            
-  CHANNEL1_NAME = "Mohanrajanrc"
-  
